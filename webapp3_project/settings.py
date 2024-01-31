0fcbfc8244ef79562d1f57a7726bb2621f369954
“John <jgbarber1981@gmail.com>
2024-01-31 14:34:10 -0600

    settings.py changed

--- webapp3_project/settings.py
+++ webapp3_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-=2t3)v%u@=8y3-o=yyoo7@%(w75z-wduk9ln4i27*e^6r_vl9s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
